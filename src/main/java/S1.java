9d1599682025b1173a8b3b7d85545a210a8b4e94
Dmitry <chernyshov85@mail.ru>
2023-02-14 05:02:09 +0300

    fix2

--- src/main/java/S1.java
+++ src/main/java/S1.java
@@ -12,22 +12,22 @@ public class S1 {
         int n = Integer.toBinaryString(i).length();
         System.out.println(n);
         // 3. Найти все кратные n числа в диапазоне от i до Short.MAX_VALUE сохранить в массив m1
-        int[] m1 = new int[Short.MAX_VALUE];
-        for (int j = i; j <= Short.MAX_VALUE; j++) {
-            if (j % n == 0)
-                m1[j] = j;
-        }
-        System.out.println(Arrays.toString(m1));
+        int count = 0;
 
-        // 4. Найти все некратные n числа в диапазоне от Short.MIN_VALUE до i и сохранить в массив m2
-        int[] m2 = new int[Short.MAX_VALUE];
-        for (int k = Short.MIN_VALUE; k <= i; k++) {
-            if (k % n != 0){
-                m2[k] = k;
+        for (int j = Short.MIN_VALUE; j < i ; j++) {
+            if (j % n == 0)
+                count++;
+        }
+        int[] m1 = new int[count];
+
+        count = 0;
+        for (int j = Short.MIN_VALUE; j < i; j++) {
+            if (j % n == 0){
+                m1[count++] = j;
             }
 
         }
-        System.out.println(Arrays.toString(m2));
+        System.out.println(Arrays.toString(m1));
     }
 
 }
