d96fd28b30c653f4e4bbe2fce9da0f0ea26746fd
Dmitry <chernyshov85@mail.ru>
2023-02-23 21:27:31 +0300

    comparison of sequential and random iteration

--- src/main/java/HW_5.java
+++ src/main/java/HW_5.java
@@ -1,48 +1,49 @@
 import java.util.HashMap;
 import java.util.TreeMap;
-
+//Создать словарь HashMap. Обобщение <Integer, String>.
+//Заполнить тремя ключами (индекс, цвет), добавить ключ, если не было!)
+//Изменить значения дописав восклицательные знаки.
+//*Создать TreeMap, заполнить аналогично.
+//*Увеличить количество элементов до 1000 случайными ключами и общей строкой.
+//**Сравнить время последовательного и случайного перебора тысячи элементов словарей.
 public class HW_5 {
     public static void main(String[] args) {
-        //Создать словарь HashMap. Обобщение <Integer, String>.
+
         HashMap<Integer, String> map = new HashMap<>();
         map.put(1, "red");
-        System.out.println(map);
-        //Заполнить тремя ключами (индекс, цвет), добавить ключь, если не было!)
         map.put(2, "yellow");
         map.put(3, "blue");
         System.out.println(map);
 
-        //Изменить значения дописав восклицательные знаки.
         map.put(1, "red!");
         map.put(2, "yellow!");
         map.put(3, "blue!");
         System.out.println(map);
-        //*Создать TreeMap, заполнить аналогично.
         TreeMap<Integer,String> trm = new TreeMap();
         trm.put(1, "red!");
         trm.put(2, "yellow!");
         trm.put(3, "blue!");
         System.out.println(trm);
-        //*Увеличить количество элементов до 1000 случайными ключами и общей строкой.
+
         for (int i = 0; i < 1000; i++){
             map.put(i, "");
         }
-        for (int z = 0; z < 1000; z++){
-            trm.put(z, "");
+        for (int j = 0; j < 1000; j++){
+            trm.put(j, "");
         }
         System.out.println(map);
         System.out.println(trm);
-        //**Сравнить время последовательного и случайного перебора тысячи элементов словарей.
-        long start1 = System.currentTimeMillis();
+
+        long start1 = System.nanoTime();
         for (Integer key: map.keySet())
             map.get(key);
-        long end1 = System.currentTimeMillis();
-        long start2 = System.currentTimeMillis();
+        long end1 = System.nanoTime();
+        long start2 = System.nanoTime();
         for (Integer key: trm.keySet())
             trm.get(key);
-        long end2 = System.currentTimeMillis();
-        System.out.println("Время перебора map: " + (end1 - start1));
-        System.out.println("Время перебора trm: " + (end2 - start2));
+        long end2 = System.nanoTime();
+        System.out.println("Время перебора map в миллисекундах: " + (end1 - start1));
+        System.out.println("Время перебора trm в миллисекундах: " + (end2 - start2));
 //
 
 
